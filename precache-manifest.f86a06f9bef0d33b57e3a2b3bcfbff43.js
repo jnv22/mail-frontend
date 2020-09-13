26e4fd1e85aa20e3d34fed41f5fb4198d97cc1fd
Jordan Vartanian <jnv22@cornell.edu>
2020-09-13 17:45:14 -0400

    Updates

---
+++ precache-manifest.f86a06f9bef0d33b57e3a2b3bcfbff43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7e8bea008cb04f116c4d17f3974581",
+    "url": "/mail-frontend/index.html"
+  },
+  {
+    "revision": "f128405b944f9707d749",
+    "url": "/mail-frontend/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "8ce42ff2320298c5e900",
+    "url": "/mail-frontend/static/js/2.28233fee.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/mail-frontend/static/js/2.28233fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f128405b944f9707d749",
+    "url": "/mail-frontend/static/js/main.48f4248c.chunk.js"
+  },
+  {
+    "revision": "66089543a6bd2bfe6d58",
+    "url": "/mail-frontend/static/js/runtime-main.1c766719.js"
+  }
+]);
